0c26dca22b7b9ff28020f078e5700fc9ca5d1920
MinPan9585 <94884915+MinPan9585@users.noreply.github.com>
2024-08-01 00:02:02 +0800

    push

--- JustinGameProject/Assets/Scripts/Bullet.cs
+++ JustinGameProject/Assets/Scripts/Bullet.cs
@@ -6,18 +6,22 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public GameObject smokepuff;
     void Update()
     {
         rb.velocity = new Vector2(0, -5);
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.tag == "Enemy")
+        if(col.gameObject.tag == "Fly")
         {
+            
             PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score") + 1);
             Debug.Log(PlayerPrefs.GetInt("score"));
             Destroy(col.gameObject);
-            Destroy(gameObject);
+            
         }
+        Destroy(gameObject);
+        Instantiate(smokepuff, transform.position, Quaternion.identity);
     }
 }
